7e87e2067be3458f5f586a4756584ca003f2de01
Yosuke Ota <otameshiyo23@gmail.com>
2021-02-23 20:45:49 +0900

    Add testcases (#10)
    * Add testcases
    
    * update

---
+++ tests/fixtures/config-test/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    extends: [
+        "plugin:@ota-meshi/recommended",
+        "plugin:@ota-meshi/+node",
+        "plugin:@ota-meshi/+typescript",
+        "plugin:@ota-meshi/+eslint-plugin",
+        "plugin:@ota-meshi/+vue2",
+        "plugin:@ota-meshi/+vue3",
+        "plugin:@ota-meshi/+json",
+        "plugin:@ota-meshi/+yaml",
+        "plugin:@ota-meshi/+toml",
+        "plugin:@ota-meshi/+md",
+        "plugin:@ota-meshi/+prettier",
+    ],
+}
