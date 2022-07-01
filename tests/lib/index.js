eeb07425245778990132bc9c37f9348b8ad7648b
JounQin <admin@1stg.me>
2022-07-01 02:37:45 +0000

    chore: use prettier-plugin-pkg and yarn

--- tests/lib/index.js
+++ tests/lib/index.js
@@ -1,47 +1,47 @@
-"use strict"
+"use strict";
 
-const path = require("path")
-const assert = require("assert")
-const { ESLint } = require("eslint")
+const path = require("path");
+const assert = require("assert");
+const { ESLint } = require("eslint");
 
-const Module = require("module")
+const Module = require("module");
 
 // -----------------------------------------------------------------------------
 // Tests
 // -----------------------------------------------------------------------------
 
-const TEST_CWD = path.join(__dirname, "../fixtures/config-test")
+const TEST_CWD = path.join(__dirname, "../fixtures/config-test");
 
 describe("Integration with @ota-meshi/eslint-plugin", () => {
-    const resolveFilename = Module._resolveFilename
-    before(() => {
-        Module._resolveFilename = function (id) {
-            if (id === "@ota-meshi/eslint-plugin") {
-                return require.resolve("../..")
-            }
-            return resolveFilename.apply(this, arguments)
-        }
-    })
+  const resolveFilename = Module._resolveFilename;
+  before(() => {
+    Module._resolveFilename = function (id) {
+      if (id === "@ota-meshi/eslint-plugin") {
+        return require.resolve("../..");
+      }
+      return resolveFilename.apply(this, arguments);
+    };
+  });
 
-    after(() => {
-        Module._resolveFilename = resolveFilename
-    })
+  after(() => {
+    Module._resolveFilename = resolveFilename;
+  });
 
-    it("should lint without errors", async () => {
-        const engine = new ESLint({
-            cwd: TEST_CWD,
-            fix: true,
-        })
-        const results = await engine.lintFiles([TEST_CWD])
-        await ESLint.outputFixes(results)
-        try {
-            assert.strictEqual(
-                results.reduce((s, r) => s + r.errorCount, 0),
-                0,
-            )
-        } catch (e) {
-            console.log(results.flatMap((r) => r.messages))
-            throw e
-        }
-    })
-})
+  it("should lint without errors", async () => {
+    const engine = new ESLint({
+      cwd: TEST_CWD,
+      fix: true,
+    });
+    const results = await engine.lintFiles([TEST_CWD]);
+    await ESLint.outputFixes(results);
+    try {
+      assert.strictEqual(
+        results.reduce((s, r) => s + r.errorCount, 0),
+        0
+      );
+    } catch (e) {
+      console.log(results.flatMap((r) => r.messages));
+      throw e;
+    }
+  });
+});
