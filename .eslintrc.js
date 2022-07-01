b456866e0f25a833fb246a879a2076e46e87ec09
JounQin <admin@1stg.me>
2022-07-01 13:59:35 +0800

    chore: use prettier-plugin-pkg and yarn (#60)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
-"use strict"
+"use strict";
 
 module.exports = {
-    extends: [
-        require.resolve("./lib/configs/recommended"),
-        require.resolve("./lib/configs/+node"),
-        require.resolve("./lib/configs/+json"),
-        require.resolve("./lib/configs/+prettier"),
-        require.resolve("./lib/configs/+package-json"),
-    ],
-    rules: {
-        "node/no-extraneous-require": "off",
-        "node/no-unpublished-require": "off",
-        "node-dependencies/no-deprecated": "error",
-    },
-}
+  extends: [
+    require.resolve("./lib/configs/recommended"),
+    require.resolve("./lib/configs/+node"),
+    require.resolve("./lib/configs/+json"),
+    require.resolve("./lib/configs/+prettier"),
+    require.resolve("./lib/configs/+package-json"),
+  ],
+  rules: {
+    "node/no-extraneous-require": "off",
+    "node/no-unpublished-require": "off",
+    "node-dependencies/no-deprecated": "error",
+  },
+};
