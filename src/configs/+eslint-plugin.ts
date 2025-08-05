673dbe0a0e033dd69cada1ff129efaffbde5234d
yosuke ota <otameshiyo23@gmail.com>
2025-08-05 11:16:41 +0900

    feat: support for eslint-plugin-eslint-plugin v7

--- src/configs/+eslint-plugin.ts
+++ src/configs/+eslint-plugin.ts
@@ -1,28 +1,38 @@
 import type { Linter } from "eslint";
-import { requireOf } from "../utils/module.js";
+import { requireFromCwd, requireOf } from "../utils/module.js";
 import {
   eslintPluginExtendRules,
   eslintPluginFiles,
 } from "../config-helpers/+eslint-plugin.js";
 import { buildFallbackForLegacy } from "./fallback.js";
+import semver from "semver";
 
 export = requireOf(
   ["eslint-plugin-eslint-plugin"],
-  (): Linter.LegacyConfig => ({
-    plugins: ["eslint-plugin"],
-    overrides: [
-      {
-        files: eslintPluginFiles,
-        extends: [
-          require.resolve("./+node"),
-          "plugin:eslint-plugin/recommended",
-        ],
-        rules: {
-          ...eslintPluginExtendRules,
+  (): Linter.LegacyConfig => {
+    const version = requireFromCwd(
+      "eslint-plugin-eslint-plugin/package.json",
+    ).version;
+    if (semver.satisfies(version, ">=7.0.0")) {
+      // eslint-plugin-eslint-plugin v7+ does not support legacy config
+      return {};
+    }
+    return {
+      plugins: ["eslint-plugin"],
+      overrides: [
+        {
+          files: eslintPluginFiles,
+          extends: [
+            require.resolve("./+node"),
+            "plugin:eslint-plugin/recommended",
+          ],
+          rules: {
+            ...eslintPluginExtendRules,
+          },
         },
-      },
-    ],
-  }),
+      ],
+    };
+  },
   (missingList) => {
     return {
       overrides: [
